3a9903e7c4f8f350829f2a171659c7b8b67a538e
AAzubarev18 <132577017+AAzubarev18@users.noreply.github.com>
2023-05-30 18:03:55 +0400

    Update Program.cs

--- TicTacToeGame/TicTacToeGame/Program.cs
+++ TicTacToeGame/TicTacToeGame/Program.cs
@@ -55,19 +55,19 @@ namespace TicTacToeGame
                     Console.WriteLine("Перерисовываем поле...откатываемся назад");
                     Thread.Sleep(2000);
                 }
-                
+
                 flag = CheckWin();
             }
             while (!flag);
 
             Console.Clear();
             Board();
-           
+
             if (flag == true && uncertianty == true)
             {
                 Console.WriteLine("Ничья");
             }
-            else if(flag == true && uncertianty == false)
+            else if (flag == true && uncertianty == false)
             {
                 Console.WriteLine("\nИгрок {0} победил", (player % 2) + 1);
             }
@@ -155,46 +155,16 @@ namespace TicTacToeGame
         private static bool CheckWin()
         {
             //Выигрышные исходы
-            if (arr[1] == arr[2] && arr[2] == arr[3])
-            {
-                return true;
-            }
-
-            else if (arr[4] == arr[5] && arr[5] == arr[6])
-            {
-                return true;
-            }
-
-            else if (arr[6] == arr[7] && arr[7] == arr[8])
-            {
-                return true;
-            }
-
-            else if (arr[1] == arr[4] && arr[4] == arr[7])
-            {
-                return true;
-            }
-
-            else if (arr[2] == arr[5] && arr[5] == arr[8])
-            {
-                return true;
-            }
-
-            else if (arr[3] == arr[6] && arr[6] == arr[9])
-            {
-                return true;
-            }
-
-            else if (arr[1] == arr[5] && arr[5] == arr[9])
-            {
-                return true;
-            }
-            else if (arr[3] == arr[5] && arr[5] == arr[7])
+            if ((arr[1] == arr[2] && arr[2] == arr[3]) || (arr[4] == arr[5] && arr[5] == arr[6]) || 
+                (arr[6] == arr[7] && arr[7] == arr[8]) || (arr[1] == arr[4] && arr[4] == arr[7]) ||
+                (arr[2] == arr[5] && arr[5] == arr[8]) || (arr[3] == arr[6] && arr[6] == arr[9]) || 
+                (arr[1] == arr[5] && arr[5] == arr[9]) || (arr[3] == arr[5] && arr[5] == arr[7]))
             {
                 return true;
             }
             // Ничья
-            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' &&
+                     arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
             {
                 uncertianty = true;
                 return true;
