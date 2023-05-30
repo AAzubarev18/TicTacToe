5a502cffb5820ef06fe3a8b638f12269faa262cf
AAzubarev18 <132577017+AAzubarev18@users.noreply.github.com>
2023-05-30 16:49:45 +0400

    Update Program.cs

--- TicTacToeGame/TicTacToeGame/Program.cs
+++ TicTacToeGame/TicTacToeGame/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text;
 using System.Threading;
 
@@ -7,18 +7,18 @@ namespace TicTacToeGame
     class Program
     {
         static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        static int player = 1; 
-        static int cell; 
-        static int flag = 0;
-
-        
+        static int player = 1;
+        static int cell;
+        static bool flag = false;
+        static bool uncertianty = false;
 
         static void Main(string[] args)
         {
+
             do
             {
                 Console.Clear();
-                Console.ForegroundColor= ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("I Игрок(X) ПРОТИВ II Игрока(O)");
                 Console.ResetColor();
 
@@ -37,7 +37,7 @@ namespace TicTacToeGame
 
                 if (arr[cell] != 'X' && arr[cell] != 'O')
                 {
-                    if (player % 2 == 0) 
+                    if (player % 2 == 0)
                     {
                         arr[cell] = 'O';
                         player++;
@@ -53,28 +53,30 @@ namespace TicTacToeGame
                     Console.WriteLine("К сожалению ячейка {0} уже занята.", cell);
                     Console.WriteLine("\n");
                     Console.WriteLine("Перерисовываем поле...откатываемся назад");
-                    Thread.Sleep(4000);
+                    Thread.Sleep(2000);
                 }
+                
                 flag = CheckWin();
             }
-            while (flag != 1 && flag != -1);
-           
+            while (!flag);
+
             Console.Clear();
             Board();
-            if (flag == 1)       
-            {
-                Console.WriteLine("\nИгрок {0} победил", (player % 2) + 1);
-            }
-            else
+           
+            if (flag == true && uncertianty == true)
             {
                 Console.WriteLine("Ничья");
             }
+            else if(flag == true && uncertianty == false)
+            {
+                Console.WriteLine("\nИгрок {0} победил", (player % 2) + 1);
+            }
             Console.ReadLine();
         }
-        
+
         private static void Board()
         {
-            
+
             String _first = String.Format("  {0}  |  {1}  |  {2}  \n", arr[1], arr[2], arr[3]);
             String _second = String.Format("  {0}  |  {1}  |  {2}  \n", arr[4], arr[5], arr[6]);
             String _third = String.Format("  {0}  |  {1}  |  {2}  \n", arr[7], arr[8], arr[9]);
@@ -98,12 +100,12 @@ namespace TicTacToeGame
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.Write(c);
                 }
-                else if (c != 'X' || c != 'O') 
+                else if (c != 'X' || c != 'O')
                 {
                     Console.ResetColor();
                     Console.Write(c);
                 }
-                
+
             }
             Console.WriteLine("_____|_____|_____|");
             Console.WriteLine("     |     |     |");
@@ -145,61 +147,62 @@ namespace TicTacToeGame
                     Console.Write(c);
                 }
             }
-           
+
             Console.WriteLine("_____|_____|_____|");
 
         }
 
-        private static int CheckWin()
+        private static bool CheckWin()
         {
-            
+            //Выигрышные исходы
             if (arr[1] == arr[2] && arr[2] == arr[3])
             {
-                return 1;
+                return true;
             }
-          
+
             else if (arr[4] == arr[5] && arr[5] == arr[6])
             {
-                return 1;
+                return true;
             }
 
             else if (arr[6] == arr[7] && arr[7] == arr[8])
             {
-                return 1;
+                return true;
             }
-           
+
             else if (arr[1] == arr[4] && arr[4] == arr[7])
             {
-                return 1;
+                return true;
             }
 
             else if (arr[2] == arr[5] && arr[5] == arr[8])
             {
-                return 1;
+                return true;
             }
 
             else if (arr[3] == arr[6] && arr[6] == arr[9])
             {
-                return 1;
+                return true;
             }
-           
+
             else if (arr[1] == arr[5] && arr[5] == arr[9])
             {
-                return 1;
+                return true;
             }
             else if (arr[3] == arr[5] && arr[5] == arr[7])
             {
-                return 1;
+                return true;
             }
-          
-            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' &&
-                arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            // Ничья
+            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
             {
-                return -1;
+                uncertianty = true;
+                return true;
             }
             else
             {
-                return 0;
+                //В процессе игры
+                return false;
             }
         }
     }
